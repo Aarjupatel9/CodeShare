93bd713596f9c073d2cc8d3216a87ad39e9c7f4f
Aarju patel <aarjupatel0111@gmail.com>
2024-08-31 14:13:36 +0530

    fix issue in image upload functionality and add all file uplaod facility

--- backend/services/s3BucketService.js
+++ backend/services/s3BucketService.js
@@ -115,7 +115,7 @@ function removeFormS3Client(input) {
 
 // nfigure multer-s3
 const multerUpload = multer({
-  limits: { fileSize: 2000000 }, // 1 mb
+  limits: { fileSize: 80000000 }, // 1 mb
   storage: multerS3({
     s3: s3Client,
     bucket: BUCKET_NAME,
